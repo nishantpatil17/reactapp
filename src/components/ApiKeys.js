993fa9ac2bf8fca1884c4a8aeffb096bf68658a2
NishantPatil17 <139233825+NishantPatil17@users.noreply.github.com>
2023-09-15 13:38:31 +0530

    Bugs Fixed

--- src/components/ApiKeys.js
+++ src/components/ApiKeys.js
@@ -1,2 +1,2 @@
 export const key = "df4e04bd9d07efdb7c483132b22a7ef1";
-export const base = "http://api.openweathermap.org/data/2.5/";
+export const base = "https://api.openweathermap.org/data/2.5/";
